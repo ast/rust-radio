236f63898ccb33f9c4c9a775f0d45d5a68a0d18d
Albin Stigo <albin.stigo@sensrad.com>
2025-06-30 14:43:40 +0200

    fm receiver scaphold

---
+++ receiver/src/bin/fm.rs
@@ -0,0 +1,74 @@
+use clap::{Parser, Subcommand};
+
+use airspyhf::Device;
+use anyhow::{Context, Result};
+
+#[derive(Parser)]
+#[command(name = "WFM receiver")]
+#[command(about = "Rust SDR receiver", long_about = None)]
+#[command(author = "Albin Stigo <albin@sm6wjm.se>")]
+#[command(version = "0.1")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Start {
+        /// Frequency in Hz
+        #[arg(short, long, default_value_t = 89_300_000.0)]
+        frequency: f64,
+        /// Sample rate
+        #[arg(short, long, default_value_t = 768_000)]
+        samplerate: u32,
+    },
+}
+
+fn main() -> Result<()> {
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Start {
+            frequency,
+            samplerate,
+        } => {
+            let mut device = Device::open().context("Failed to open device")?;
+
+            // Set frequency
+            device
+                .set_frequency(*frequency)
+                .context("Failed to set frequency")?;
+
+            // Set samplerate
+            device
+                .set_samplerate(*samplerate)
+                .context("Failed to set samplerate")?;
+
+            // Start streaming without recording
+            device
+                .start(move |samples, dropped| {
+                    if dropped > 0 {
+                        eprintln!("Dropped {} samples", dropped);
+                    }
+
+                    println!("Received {} samples", samples.len());
+
+                    0 // = continue
+                })
+                .context("Failed to start device")?;
+
+            // Wait for user input to stop
+            println!("Press Enter to stop...");
+            let mut input = String::new();
+            std::io::stdin()
+                .read_line(&mut input)
+                .context("Failed to read input")?;
+
+            // Stop streaming
+            device.stop().context("Failed to stop device")?;
+        }
+    }
+
+    Ok(())
+}
